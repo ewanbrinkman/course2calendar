086822f67978327651e88f87eae3725e0eec2fa9
Ewan Brinkman <ewanbrinkman@gmail.com>
2025-01-05 18:04:26 -1000

    Clean up term selector combobox style
    Don't use `style` and instead use `styles` for style.

--- src/app/components/TermSelector/TermSelectorCombobox/TermSelectorCombobox.tsx
+++ src/app/components/TermSelector/TermSelectorCombobox/TermSelectorCombobox.tsx
@@ -41,54 +41,60 @@ export default function TermSelectorCombobox<T>(
   return (
     <div>
       <Text size="sm">{props.label}</Text>
-      <div className={error ? classes.error_input : ""}>
-        <Combobox
-          store={combobox}
-          onOptionSubmit={(value) => {
-            setValue(value);
-            props.onOptionSubmit(value);
-            combobox.closeDropdown();
-          }}
-        >
-          <Combobox.Target>
-            <InputBase
-              component="button"
-              type="button"
-              disabled={isDisabled}
-              pointer
-              rightSection={<Combobox.Chevron />}
-              rightSectionPointerEvents={isDisabled ? "none" : "all"}
-              onClick={() => {
-                if (!isDisabled) {
-                  combobox.toggleDropdown();
-                }
-              }}
-              styles={{
-                input: {
-                  cursor: isDisabled ? "not-allowed" : "pointer",
-                  opacity: isDisabled ? 0.6 : 1,
-                  borderColor: error ? "var(--mantine-color-error)" : undefined,
-                },
-              }}
-            >
-              {value || (
-                <Input.Placeholder>{props.placeholder}</Input.Placeholder>
-              )}
-            </InputBase>
-          </Combobox.Target>
+      <Combobox
+        store={combobox}
+        onOptionSubmit={(value) => {
+          setValue(value);
+          props.onOptionSubmit(value);
+          combobox.closeDropdown();
+        }}
+      >
+        <Combobox.Target>
+          <InputBase
+            component="button"
+            type="button"
+            disabled={isDisabled}
+            pointer
+            rightSection={<Combobox.Chevron />}
+            rightSectionPointerEvents={isDisabled ? "none" : "all"}
+            onClick={() => {
+              if (!isDisabled) {
+                combobox.toggleDropdown();
+              }
+            }}
+            styles={{
+              input: {
+                cursor: isDisabled ? "not-allowed" : "pointer",
+                opacity: isDisabled ? 0.6 : 1,
+                borderColor: error ? "var(--mantine-color-error)" : undefined,
+                marginBottom: error
+                  ? "calc(var(--mantine-spacing-xs) / 2)"
+                  : undefined,
+              },
+            }}
+          >
+            {value || (
+              <Input.Placeholder>{props.placeholder}</Input.Placeholder>
+            )}
+          </InputBase>
+        </Combobox.Target>
 
-          {!isDisabled && (
-            <Combobox.Dropdown>
-              <Combobox.Options>{options}</Combobox.Options>
-            </Combobox.Dropdown>
-          )}
-        </Combobox>
-      </div>
+        {!isDisabled && (
+          <Combobox.Dropdown>
+            <Combobox.Options>{options}</Combobox.Options>
+          </Combobox.Dropdown>
+        )}
+      </Combobox>
       {error && (
         <Text
           className="errorText"
           size="xs"
-          style={{ color: "var(--mantine-color-error)" }}
+          styles={{
+            root: {
+              color: "var(--mantine-color-error)",
+            },
+          }}
+          // style={{ color: "var(--mantine-color-error)" }}
         >
           {error}
         </Text>
